6e0e42718052ab59e3121a87ad13d56eeee7801b
Danylo_Ikol <daniil.ikol@gmail.com>
2023-07-14 09:07:28 +0300

    Refactor to use webhook instead of pulling bot

--- src/main/java/web/telegram/bot/clicker/telegram/WebhookController.java
+++ src/main/java/web/telegram/bot/clicker/telegram/WebhookController.java
@@ -1,6 +1,7 @@
 package web.telegram.bot.clicker.telegram;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,15 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @AllArgsConstructor
 public class WebhookController {
     private final CorruptedFilesAssistanceBot bot;
+
     @PostMapping("/")
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
         return bot.onWebhookUpdateReceived(update);
     }
+
+    @GetMapping("/")
+    public String onUpdateReceived() {
+        return "hello from oracle";
+    }
+
 }
